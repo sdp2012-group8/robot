6c30dcdef6d73de73964f7a75cc8d5768d1459d7
Gediminas Liktaras <gliktaras@gmail.com>
2012-01-28 20:17:52 +0000

    Started replacing old GUI with the new code.
    Changes:
    > Removed GUI.form
    > Replaced Swing GroupLayout with something that does not vomit all
      over the code.

--- robot-GUI/src/sdp/gui/OldGUI.java
+++ robot-GUI/src/sdp/gui/OldGUI.java
@@ -2,84 +2,49 @@ package sdp.gui;
 
 import java.awt.image.BufferedImage;
 
-import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.SwingConstants;
 
+/**
+ * The GUI class of the main window.
+ */
 public class OldGUI extends javax.swing.JFrame{
 	
+	/** Required by Serializable. */
+	private static final long serialVersionUID = 8597348579639499324L;
 	public static String className = null;
 
 	public OldGUI(){
+		setTitle("Battlestation");
 		initComponents();
 	}
 	
-	private void initComponents(){
-		
-		
-		onScreenImage = new javax.swing.JLabel();
+	private void initComponents() {
+		imageLabel = new javax.swing.JLabel();
+		imageLabel.setAlignmentY(Component.TOP_ALIGNMENT);
 		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-		setName("Vision"); // NOI18N
-		setResizable(false);
+		setName("Vision");
+		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
 
-		onScreenImage.setText("Image goes here");
-		
-		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
-				getContentPane());
-		getContentPane().setLayout(layout);
-		layout
-				.setHorizontalGroup(layout
-						.createParallelGroup(
-								javax.swing.GroupLayout.Alignment.LEADING)
-						.addGroup(
-								layout
-										.createSequentialGroup()
-										.addComponent(
-												onScreenImage,
-												javax.swing.GroupLayout.PREFERRED_SIZE,
-												640,
-												javax.swing.GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(
-												javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-										
-										.addContainerGap()));
-		layout
-				.setVerticalGroup(layout
-						.createParallelGroup(
-								javax.swing.GroupLayout.Alignment.LEADING)
-						.addGroup(
-								layout
-										.createSequentialGroup()
-										.addGroup(
-												layout
-														.createParallelGroup(
-																javax.swing.GroupLayout.Alignment.LEADING)
-							
-														.addComponent(
-																onScreenImage,
-																javax.swing.GroupLayout.PREFERRED_SIZE,
-																480,
-																javax.swing.GroupLayout.PREFERRED_SIZE))
-										.addPreferredGap(
-												javax.swing.LayoutStyle.ComponentPlacement.RELATED,
-												javax.swing.GroupLayout.DEFAULT_SIZE,
-												Short.MAX_VALUE)
-										.addContainerGap()));
-
-		pack();
-		
-		
-		
+		imageLabel.setText("Image goes here");
+		getContentPane().add(imageLabel);
 	}
 	
-	private javax.swing.JLabel onScreenImage;
+	private javax.swing.JLabel imageLabel;
 	
 	public javax.swing.JLabel getImage() {
-		return onScreenImage;
+		return imageLabel;
 	}
 	
 	public void setImage(BufferedImage image) {
-		if (image != null)
-			onScreenImage.getGraphics().drawImage(image, 0, 0,
-					image.getWidth(), image.getHeight(), null);
+		if (image != null) {
+			imageLabel.setSize(image.getWidth(), image.getHeight());
+			imageLabel.getGraphics().drawImage(image, 0, 0, null);
+		}
 	}
 	
 }
