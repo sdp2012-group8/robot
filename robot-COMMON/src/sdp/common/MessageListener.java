ff5d5f20aa7de2f7733ba1ad0b3dc03d6fb60dc8
Martin Marinov <martintzvetomirov@abv.bg>
2012-01-28 14:55:10 +0000

    Fixed some missing files. Now AI should be the same as master

---
+++ robot-COMMON/src/sdp/common/MessageListener.java
@@ -0,0 +1,21 @@
+package sdp.common;
+
+import sdp.common.Communicator.opcode;
+
+/**
+ * Implement this interface if you want to receive messages from a {@link Communicator}
+ * 
+ * @author martinmarinov
+ *
+ */
+public interface MessageListener {
+	
+	/**
+	 * This method will be called when a new message is available.
+	 * @param op the opcode of the message
+	 * @param args the arguments of the message
+	 * @param controler which controller is returning the result
+	 */
+	public void receiveMessage(opcode op, byte[] args, Communicator controler);
+
+}
