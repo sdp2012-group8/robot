139a8f8237f8bbc2528c21e36c9ce28abfeab609
Gediminas Liktaras <gliktaras@gmail.com>
2012-02-10 20:03:04 +0000

    Added FPS option to test mode.

--- robot-GUI/src/sdp/gui/Launcher.java
+++ robot-GUI/src/sdp/gui/Launcher.java
@@ -20,8 +20,9 @@ import javax.swing.JSpinner;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingUtilities;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.Dimension;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
@@ -43,19 +44,20 @@ public class Launcher extends JFrame implements Runnable {
 	private JComboBox standardComboBox;	
 	/** A spinner for selecting the camera channel. */
 	private JSpinner channelSpinner;
+	private JSpinner testFpsSpinner;
 	
 	
 	/**
 	 * The main constructor.
 	 */
 	public Launcher() {
-		setSize(new Dimension(198, 215));
+		setSize(new Dimension(198, 243));
 		setTitle("Launcher");
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{150, 0};
 		gridBagLayout.rowHeights = new int[]{25, 0, 0};
 		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
 		getContentPane().setLayout(gridBagLayout);
 		
 		JPanel competitionModePanel = new JPanel();
@@ -68,7 +70,7 @@ public class Launcher extends JFrame implements Runnable {
 		getContentPane().add(competitionModePanel, gbc_competitionModePanel);
 		competitionModePanel.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Competition mode", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(51, 51, 51)));
 		GridBagLayout gbl_competitionModePanel = new GridBagLayout();
-		gbl_competitionModePanel.columnWidths = new int[]{0, 0, 0};
+		gbl_competitionModePanel.columnWidths = new int[]{80, 0, 0};
 		gbl_competitionModePanel.rowHeights = new int[]{0, 0, 0, 0, 0};
 		gbl_competitionModePanel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
 		gbl_competitionModePanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
@@ -137,35 +139,54 @@ public class Launcher extends JFrame implements Runnable {
 		gbc_startCompButton.gridy = 3;
 		competitionModePanel.add(startCompButton, gbc_startCompButton);
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Test mode", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.anchor = GridBagConstraints.NORTH;
-		gbc_panel.fill = GridBagConstraints.HORIZONTAL;
-		gbc_panel.gridx = 0;
-		gbc_panel.gridy = 1;
-		getContentPane().add(panel, gbc_panel);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0};
-		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
+		JPanel testModePanel = new JPanel();
+		testModePanel.setBorder(new TitledBorder(null, "Test mode", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		GridBagConstraints gbc_testModePanel = new GridBagConstraints();
+		gbc_testModePanel.anchor = GridBagConstraints.NORTH;
+		gbc_testModePanel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_testModePanel.gridx = 0;
+		gbc_testModePanel.gridy = 1;
+		getContentPane().add(testModePanel, gbc_testModePanel);
+		GridBagLayout gbl_testModePanel = new GridBagLayout();
+		gbl_testModePanel.columnWidths = new int[]{80, 0, 0};
+		gbl_testModePanel.rowHeights = new int[]{0, 0, 0};
+		gbl_testModePanel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_testModePanel.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		testModePanel.setLayout(gbl_testModePanel);
+		
+		JLabel testFpsLabel = new JLabel("FPS");
+		GridBagConstraints gbc_testFpsLabel = new GridBagConstraints();
+		gbc_testFpsLabel.anchor = GridBagConstraints.EAST;
+		gbc_testFpsLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_testFpsLabel.gridx = 0;
+		gbc_testFpsLabel.gridy = 0;
+		testModePanel.add(testFpsLabel, gbc_testFpsLabel);
+		
+		testFpsSpinner = new JSpinner();
+		testFpsSpinner.setModel(new SpinnerNumberModel(25, 1, 60, 1));
+		GridBagConstraints gbc_testFpsSpinner = new GridBagConstraints();
+		gbc_testFpsSpinner.fill = GridBagConstraints.HORIZONTAL;
+		gbc_testFpsSpinner.insets = new Insets(0, 0, 5, 0);
+		gbc_testFpsSpinner.gridx = 1;
+		gbc_testFpsSpinner.gridy = 0;
+		testModePanel.add(testFpsSpinner, gbc_testFpsSpinner);
 		
 		JButton startTestButton = new JButton("Start testing");
 		GridBagConstraints gbc_startTestButton = new GridBagConstraints();
+		gbc_startTestButton.gridwidth = 2;
+		gbc_startTestButton.insets = new Insets(0, 0, 0, 5);
 		gbc_startTestButton.gridx = 0;
-		gbc_startTestButton.gridy = 0;
-		panel.add(startTestButton, gbc_startTestButton);
-		startTestButton.addMouseListener(new MouseAdapter() {
+		gbc_startTestButton.gridy = 1;
+		testModePanel.add(startTestButton, gbc_startTestButton);
+		startTestButton.addActionListener(new ActionListener() {			
 			@Override
-			public void mouseClicked(MouseEvent e) {
+			public void actionPerformed(ActionEvent e) {
 				startTestingMode();
 			}
 		});
-		startCompButton.addMouseListener(new MouseAdapter() {
+		startCompButton.addActionListener(new ActionListener() {			
 			@Override
-			public void mouseClicked(MouseEvent e) {
+			public void actionPerformed(ActionEvent e) {
 				startCompetitionMode();
 			}
 		});
@@ -235,7 +256,9 @@ public class Launcher extends JFrame implements Runnable {
 				"../robot-VISION/data/testImages/pitch2-2.png",
 				"../robot-VISION/data/testImages/pitch2-3.png"
 		};
-		return new ImageVisualInputProvider(filenames, 25);
+		int fps = ((Integer)testFpsSpinner.getValue()).intValue();
+		
+		return new ImageVisualInputProvider(filenames, fps);
 	}
 	
 	/**
