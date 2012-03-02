4601fcad1954e10da5639975bcecd096c38a3717
MJohnson459 <mjohnson459@gmail.com>
2012-03-02 15:01:47 +0000

    Added find optimal point behind ball

--- robot-AI/src/sdp/AI/AIWorldState.java
+++ robot-AI/src/sdp/AI/AIWorldState.java
@@ -1,6 +1,9 @@
 package sdp.AI;
 
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import sdp.common.Goal;
 import sdp.common.Robot;
@@ -10,22 +13,24 @@ import sdp.common.Vector2D;
 import sdp.common.WorldState;
 
 public class AIWorldState extends WorldState {
-	
+
+	private static final double POINT_OFFSET = 2*Robot.LENGTH_CM;
+
 	private boolean my_team_blue;
 	private boolean my_goal_left;
 	private Goal enemy_goal;
 	private Goal my_goal;
 	private final static double GOAL_SIZE = 60; // cm
-	
+
 	//changing variables
 	private Robot robot = null;
 	private Robot enemy_robot = null;
 	private double distance_to_ball;
 	private double distance_to_goal;
-	
+
 	//flags
 	boolean f_ball_on_field = false;
-	
+
 	//imaginary top pitch
 	private Point2D.Double[] imaginaryTopPitch = {new Point2D.Double(0,0),
 			new Point2D.Double(Tools.PITCH_WIDTH_CM,0),new Point2D.Double(Tools.PITCH_WIDTH_CM,-Tools.PITCH_HEIGHT_CM),
@@ -37,17 +42,17 @@ public class AIWorldState extends WorldState {
 	//imaginary top enemy goal
 	private Point2D.Double[] imaginaryTopEnemyGoal = {new Point2D.Double(0,0), new Point2D.Double(0,0)};
 	private Point2D.Double[] imaginaryBottomEnemyGoal = {new Point2D.Double(0,0), new Point2D.Double(0,0)};
-	
-		
+
+
 	public AIWorldState(WorldState world_state, boolean my_team_blue, boolean my_goal_left) {
 		super(world_state.getBallCoords(), world_state.getBlueRobot(),world_state.getYellowRobot(), world_state.getWorldImage());
 
-		
+
 		update(world_state, my_team_blue, my_goal_left);
 	}
-	
+
 	public void update(WorldState world_state, boolean my_team_blue, boolean my_goal_left) {
-		
+
 		this.my_team_blue = my_team_blue;
 		this.my_goal_left = my_goal_left;
 
@@ -58,7 +63,7 @@ public class AIWorldState extends WorldState {
 			enemy_goal = new Goal(new Point2D.Double(0 , Tools.GOAL_Y_CM ));
 			my_goal = new Goal(new Point2D.Double(Tools.PITCH_WIDTH_CM , Tools.GOAL_Y_CM ));
 		}
-				
+
 		super.update(world_state.getBallCoords(), world_state.getBlueRobot(),world_state.getYellowRobot(), world_state.getWorldImage());
 		//update variables
 		if (my_team_blue) {
@@ -68,47 +73,53 @@ public class AIWorldState extends WorldState {
 			robot = (getYellowRobot());
 			enemy_robot = (getBlueRobot());
 		}
-		
-		
+
+
 		distance_to_ball = Tools.getDistanceBetweenPoint(Tools.getGlobalVector(robot, new Vector2D(Robot.LENGTH_CM/2, 0)), getBallCoords());
 		distance_to_goal = Tools.getDistanceBetweenPoint(robot.getCoords(), enemy_goal.getCentre());
-		
+
 		// check and set flags
 		if (getBallCoords() == new Point2D.Double(-1,-1)) {
 			f_ball_on_field = false;
 		}
 	}
-	
+
+
+
+	////////////////////////////////////////////////////////////////
+	// Methods
+	///////////////////////////////////////////////////////////////
+
 
 	/**
 	 * Calculates if the ball has a direct line of sight to the enemy goal.
 	 * @return true if goal is visible
 	 */
 	public boolean isGoalVisible() {
-		
+
 		enemy_robot.setCoords(true); //need to convert robot coords to cm
-		
+
 		if (Utilities.isPathClear(getBallCoords(), enemy_goal.getCentre(), enemy_robot)
 				|| Utilities.isPathClear(getBallCoords(), enemy_goal.getTop(), enemy_robot) 
 				|| Utilities.isPathClear(getBallCoords(), enemy_goal.getBottom(), enemy_robot)) {
 			return true;
 		}
-		
+
 		//Check for "special" case where the ball is between the enemy robot and their goal.
 		if (enemy_goal.getCentre().x == 0) {
 			if (getBallCoords().x < enemy_robot.getCoords().x) return true;
 		} else {
 			if (getBallCoords().x > enemy_robot.getCoords().x) return true;
 		}
-		
+
 		return false; //can't see goal
 	}
-	
+
 	public double calculateShootAngle() {
 		double topmin = anglebetween(getRobot().getCoords(), enemy_goal.getTop());
 		double midmin = anglebetween(getRobot().getCoords(), enemy_goal.getCentre());
 		double botmin = anglebetween(getRobot().getCoords(), enemy_goal.getBottom());
-		
+
 		if (Math.abs(topmin) < Math.abs(midmin) && Math.abs(topmin) < Math.abs(botmin) && Utilities.isPathClear(getBallCoords(), enemy_goal.getTop(), enemy_robot)) {
 			return topmin;
 		} else if (Math.abs(midmin) < Math.abs(botmin) && Utilities.isPathClear(getBallCoords(), enemy_goal.getCentre(), enemy_robot)) {
@@ -116,9 +127,9 @@ public class AIWorldState extends WorldState {
 		} else {
 			return botmin;
 		}
-		
+
 	}
-	
+
 	/**
 	 * This checks if the robot can see the imaginary goals
 	 * If it returns true, the robot can kick the ball into the wall and it should reflect towards the enemy goal
@@ -131,7 +142,7 @@ public class AIWorldState extends WorldState {
 			imaginaryTopEnemyGoal[1] = new Point2D.Double(Tools.PITCH_WIDTH_CM,-Tools.PITCH_HEIGHT_CM/2+GOAL_SIZE/2); //bottom point
 			imaginaryBottomEnemyGoal[0] = new Point2D.Double(Tools.PITCH_WIDTH_CM,3*Tools.PITCH_HEIGHT_CM/2-GOAL_SIZE/2); //top point
 			imaginaryBottomEnemyGoal[1] = new Point2D.Double(Tools.PITCH_WIDTH_CM,3*Tools.PITCH_HEIGHT_CM/2+GOAL_SIZE/2); //bottom point
-			
+
 		}
 		else {
 			imaginaryTopEnemyGoal[0] = new Point2D.Double(0,-Tools.PITCH_HEIGHT_CM/2-GOAL_SIZE/2); //top point
@@ -139,7 +150,7 @@ public class AIWorldState extends WorldState {
 			imaginaryBottomEnemyGoal[0] = new Point2D.Double(0,3*Tools.PITCH_HEIGHT_CM/2-GOAL_SIZE/2); //top point
 			imaginaryBottomEnemyGoal[1] = new Point2D.Double(0,3*Tools.PITCH_HEIGHT_CM/2+GOAL_SIZE/2); //bottom point
 		}
-		
+
 		//robot in upper half of pitch, play ball with top wall
 		if (getRobot().getCoords().y < Tools.PITCH_HEIGHT_CM/2){ 
 			Point2D.Double intersection = Utilities.intersection(getBallCoords(), getRobot().getCoords(), imaginaryTopEnemyGoal[0], imaginaryTopEnemyGoal[1]);
@@ -163,10 +174,10 @@ public class AIWorldState extends WorldState {
 				}
 			}	
 		}
-		
+
 		return false;
 	}
-	
+
 	/**
 	 * Gets the angle between two points
 	 * @param A
@@ -177,6 +188,73 @@ public class AIWorldState extends WorldState {
 		return (180*Math.atan2(-B.getY()+A.getY(), B.getX()-A.getX()))/Math.PI;
 	}
 
+
+	/**
+	 * Returns the point the robot should go to behind the ball.
+	 * Distance behind the ball set by POINT_OFFSET
+	 * @param point The target point on the goal the robot should be aligned to.
+	 * @return Point2D.Double behind the ball
+	 */
+	public Point2D.Double getPointBehindBall(Point2D.Double point) {
+		Point2D.Double ball = getBallCoords();
+
+		if (point.getY() == ball.getY()) {
+			return new Point2D.Double(my_goal_left ? ball.getX() - POINT_OFFSET : ball.getX() + POINT_OFFSET, ball.getY());
+		} else {
+			double x, y, a, b;
+			a = point.getY() - ball.getY();
+			b = point.getX() - ball.getX();
+
+			if (my_goal_left) {
+				y = ball.getY() - POINT_OFFSET*a/(Math.sqrt(b*b + a*a));
+			} else {
+				y = ball.getY() + POINT_OFFSET*a/(Math.sqrt(b*b + a*a));
+			}
+
+			x = ball.getX() + (b*(y - ball.getY())/a);
+
+			return new Point2D.Double(x,y);
+		}
+	}
+
+
+	public Point2D.Double getOptimalPointBehindBall() {
+		ArrayList<Point2D.Double> goal_points = new ArrayList<Point2D.Double>();
+		Point2D.Double min_point = null;
+		double min_distance = Tools.PITCH_WIDTH_CM*2;
+
+		goal_points.add(enemy_goal.getCentre());
+		goal_points.add(enemy_goal.getTop());
+		goal_points.add(enemy_goal.getBottom());
+		goal_points.add(new Point2D.Double(enemy_goal.getCentre().x, enemy_goal.getCentre().y - Tools.PITCH_HEIGHT_CM));
+		goal_points.add(new Point2D.Double(enemy_goal.getCentre().x, enemy_goal.getCentre().y + Tools.PITCH_HEIGHT_CM));
+
+		Iterator<Point2D.Double> itr = goal_points.iterator();
+		while (itr.hasNext()) {
+			Point2D.Double point = itr.next();
+			if (!Utilities.isPathClear(point, getBallCoords(), enemy_robot)) {
+				itr.remove();
+			}
+		}
+
+		itr = goal_points.iterator();
+		while (itr.hasNext()) {
+			Point2D.Double point = itr.next();
+			Point2D.Double temp_point = getPointBehindBall(point);
+			if (Vector2D.subtract(new Vector2D(temp_point), new Vector2D(robot.getCoords())).getLength() < min_distance) {
+				min_point = temp_point;
+			}
+		}
+		
+		System.out.println(min_point);
+
+		return min_point;
+	}
+
+
+	////////////////////////////////////////////////////////
+	// getters and setters
+	///////////////////////////////////////////////////////
 	public Robot getEnemyRobot() {
 		return enemy_robot;
 	}
@@ -184,11 +262,11 @@ public class AIWorldState extends WorldState {
 	public Robot getRobot() {
 		return robot;
 	}
-	
+
 	public double getDistanceToBall() {
 		return distance_to_ball;
 	}
-	
+
 	public double getDistanceToGoal() {
 		return distance_to_goal;
 	}
@@ -196,7 +274,7 @@ public class AIWorldState extends WorldState {
 	public Goal getEnemyGoal() {
 		return enemy_goal;
 	}
-	
+
 	public Goal getMyGoal() {
 		return my_goal;
 	}
@@ -204,7 +282,7 @@ public class AIWorldState extends WorldState {
 	public boolean getMyGoalLeft() {
 		return my_goal_left;
 	}
-	
+
 	public boolean getMyTeamBlue() {
 		return my_team_blue;
 	}
