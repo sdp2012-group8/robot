1c71ac48decb269f21f7a0753a441ddac35a3b9e
Gediminas Liktaras <gliktaras@gmail.com>
2012-03-02 21:58:50 +0000

    Added direction detection to test bench.

--- robot-VISION/src/sdp/vision/testbench/VisionSystemErrorAccumulator.java
+++ robot-VISION/src/sdp/vision/testbench/VisionSystemErrorAccumulator.java
@@ -22,12 +22,20 @@ public class VisionSystemErrorAccumulator {
 
 	/** A list with accumulated test names. */
 	private ArrayList<String> testNames = new ArrayList<String>();
+	/** Execution time accumulator. */
+	private ArrayList<Long> testTimes = new ArrayList<Long>();
+
 	/** Ball position measurement error accumulator. */
 	private PositionErrorAccumulator ballPosError = new PositionErrorAccumulator();
 	/** Blue robot position measurement error accumulator. */
 	private PositionErrorAccumulator bluePosError = new PositionErrorAccumulator();
 	/** Yellow robot position measurement error accumulator. */
 	private PositionErrorAccumulator yellowPosError = new PositionErrorAccumulator();
+	
+	/** Blue robot direction measurement error accumulator. */
+	private DirectionErrorAccumulator blueDirError = new DirectionErrorAccumulator();
+	/** Yellow robot direction measurement error accumulator. */
+	private DirectionErrorAccumulator yellowDirError = new DirectionErrorAccumulator();
 	
 	
 	/**
@@ -43,15 +51,23 @@ public class VisionSystemErrorAccumulator {
 	 * 
 	 * @param expected Expected world state *in frame coordinates*.
 	 * @param actual Actual world state *in frame coordinates*.
+	 * @param time Time the system took to process the frame, in ms.
 	 */
-	public void addRecord(String testName, WorldState expected, WorldState actual) {
+	public void addRecord(String testName, WorldState expected, WorldState actual,
+			long time) {
 		testNames.add(testName);
+		testTimes.add(time);
 		
 		ballPosError.addRecord(expected.getBallCoords(), actual.getBallCoords());
 		bluePosError.addRecord(expected.getBlueRobot().getCoords(),
 				actual.getBlueRobot().getCoords());
 		yellowPosError.addRecord(expected.getYellowRobot().getCoords(),
 				actual.getYellowRobot().getCoords());
+		
+		blueDirError.addRecord(expected.getBlueRobot().getAngle(),
+				actual.getBlueRobot().getAngle());
+		yellowDirError.addRecord(expected.getYellowRobot().getAngle(),
+				actual.getYellowRobot().getAngle());
 	}
 	
 	
@@ -61,6 +77,9 @@ public class VisionSystemErrorAccumulator {
 	 * @param out Output stream to dump to.
 	 */
 	public void dumpMetrics(PrintStream out) {
+		double avgExecTime = getAverageExecutionTime();
+		double avgFps = 1000.0 / avgExecTime;
+		
 		out.format("=== TEST SUMMARY\n");
 		out.format("\n");
 		out.format("Number of tests: %d\n", testNames.size());
@@ -76,17 +95,31 @@ public class VisionSystemErrorAccumulator {
 				bluePosError.getInvalidRecordCount(),
 				bluePosError.getInaccurateRecordCount(),
 				bluePosError.getAccurateRecordCount());
+		out.format("  Blue robot direction: %d/%d/%d\n",
+				blueDirError.getInvalidRecordCount(),
+				blueDirError.getInaccurateRecordCount(),
+				blueDirError.getAccurateRecordCount());
 		out.format("  Yellow robot position: %d/%d/%d\n",
 				yellowPosError.getInvalidRecordCount(),
 				yellowPosError.getInaccurateRecordCount(),
 				yellowPosError.getAccurateRecordCount());
+		out.format("  Yellow robot direction: %d/%d/%d\n",
+				yellowDirError.getInvalidRecordCount(),
+				yellowDirError.getInaccurateRecordCount(),
+				yellowDirError.getAccurateRecordCount());
+		
 		out.format("\n");
+		out.format("Average execution time: %.2f ms, %.2f fps.\n", avgExecTime, avgFps);
 		out.format("Average ball position error: %.4f pixels.\n",
 				ballPosError.averageError());
 		out.format("Average blue robot position error: %.4f pixels.\n",
 				bluePosError.averageError());
+		out.format("Average blue robot direction error: %.4f degrees.\n",
+				blueDirError.averageError());
 		out.format("Average yellow robot position error: %.4f pixels.\n",
 				yellowPosError.averageError());
+		out.format("Average yellow robot direction error: %.4f degrees.\n",
+				yellowDirError.averageError());
 		
 		out.format("\n");
 		out.format("\n");
@@ -94,15 +127,22 @@ public class VisionSystemErrorAccumulator {
 		for (int i = 0; i < testNames.size(); ++i) {
 			out.format("\n");
 			out.format("Test image: %s\n", testNames.get(i));
+			out.format("  Execution time: %d ms\n", testTimes.get(i));
 			out.format("  Ball position error: %.4f pixels - %s\n",
 					ballPosError.getRecord(i),
 					posErrorMessage(ballPosError.getRecord(i)));
 			out.format("  Blue position error: %.4f pixels - %s\n",
 					bluePosError.getRecord(i),
 					posErrorMessage(bluePosError.getRecord(i)));
+			out.format("  Blue direction error: %.4f pixels - %s\n",
+					blueDirError.getRecord(i),
+					posErrorMessage(blueDirError.getRecord(i)));
 			out.format("  Yellow position error: %.4f pixels - %s\n",
 					yellowPosError.getRecord(i),
 					posErrorMessage(yellowPosError.getRecord(i)));
+			out.format("  Yellow direction error: %.4f pixels - %s\n",
+					yellowDirError.getRecord(i),
+					posErrorMessage(yellowDirError.getRecord(i)));
 		}
 	}
 	
@@ -119,6 +159,24 @@ public class VisionSystemErrorAccumulator {
 			return "INACCURATE";
 		} else {
 			return "OK";
+		}
+	}
+	
+	/**
+	 * Get average test execution time.
+	 * 
+	 * @return Average time in milliseconds.
+	 */
+	private double getAverageExecutionTime() {
+		double sum = 0.0;
+		for (long t : testTimes) {
+			sum += t;
+		}
+		
+		if (testTimes.size() == 0) {
+			return -1.0;
+		} else {
+			return sum / testTimes.size();
 		}
 	}
 
