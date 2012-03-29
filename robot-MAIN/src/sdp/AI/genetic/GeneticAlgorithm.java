b47dd240eca0197906f201a2c6015122b6ca9d9f
MJohnson459 <mjohnson459@gmail.com>
2012-03-29 17:39:08 +0100

    updated genetic algorithms

--- robot-MAIN/src/sdp/AI/genetic/GeneticAlgorithm.java
+++ robot-MAIN/src/sdp/AI/genetic/GeneticAlgorithm.java
@@ -1,34 +1,39 @@
 package sdp.AI.genetic;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 import java.io.*;
 
 
 public class GeneticAlgorithm {
 
-	final int runs = 1;
-	final int generations = 1000;
-	final int memory = 5;
-	final int games = 5;
-
-	final int popSize = 50;
-	final double crossProb = 0.6;
-	final double mutateProb = 0.001;
-
-	int T = 5;	
-	int C = 3;
-	int D = 1;
-	int S = 0;
+	/** Number of generations to simulate */
+	final static int GENERATIONS = 50;
+	/** Number of games each individual plays against every other */
+	final static int GAMES = 1;
+	/** Size of the population */
+	final static int POPSIZE = 20;
+	/** Probability that a crossover will occur */
+	final static float CROSSOVER_PROB = 0.6F;
+	/** Probability that a mutation will occur */
+	final static float MUTATE_PROB = 0.001F;
+	/** Number of genes in each individual */
+	final static int GENE_NUMBER = 100;
+	/** Number of neighbours each individual plays against */
+	final static int NEIGHBOUR_NUMBER = 10;
 
 	int gen = 0;
-	double fitTotal = 0;
-	double[] popFitness;
-	double[] avgFitness;
-	int[][] population;
-	int[][] intPopulation;
-	int[][][] finalPopulations;
-	double[][] finalPopFitness;
+	long fitTotal = 0;
+	long[] popFitness;
+	long[] avgFitness;
+	Double[][] population;
+	Double[][] intPopulation;
+	Double[][] finalPopulation;
+	double[] finalPopFitness;
 	Random rand = new Random();
-	int length = (int) Math.pow(2,(memory*2)) + 2*memory;
+
 
 	FileWriter fstream;
 	PrintWriter out;
@@ -48,56 +53,53 @@ public class GeneticAlgorithm {
 	public GeneticAlgorithm() throws IOException {
 		fstream = new FileWriter("out.txt");
 		out = new PrintWriter(fstream);
-		finalPopulations = new int[runs][popSize][length];
-		finalPopFitness = new double[runs][popSize+1];
+		finalPopulation = new Double[POPSIZE][GENE_NUMBER];
+		finalPopFitness = new double[POPSIZE+1];
 
-		for (int i = 0; i < runs; i++) {	
-			System.out.println("Run " + (i+1));
-			setup();
-			out.println("Run " + (i+1));
-			out.println("\nInitial population\n");
+
+		setup();
+		out.println("\nInitial population\n");
+		printPop();
+
+		for (gen = 1; gen < GENERATIONS+1; gen++) {
+			run();
+			out.println("\nGeneration " + gen);
+
 			printPop();
-
-			for (gen = 1; gen < generations+1; gen++) {
-				run();
-				out.println("\nGeneration " + gen);
-
-				printPop();
-			}
-			System.arraycopy(population, 0, finalPopulations[i], 0, popSize);
-			System.arraycopy(popFitness, 0, finalPopFitness[i], 0, popSize);
-			finalPopFitness[i][popSize] = avgFitness[gen-1];
-			out.println("\n\n");
 		}
+		System.arraycopy(population, 0, finalPopulation, 0, POPSIZE);
+		System.arraycopy(popFitness, 0, finalPopFitness, 0, POPSIZE);
+		finalPopFitness[POPSIZE] = avgFitness[gen-1];
+		out.println("\n\n");
+
 		out.close();
 
 		fstream = new FileWriter("finalPopulations.txt");
 		out = new PrintWriter(fstream);
 
-		out.println("Final Populations\n");
-		for (int i = 0; i < runs; i++) {
-			out.println("Run " + (i+1));
-			for (int j = 0; j < popSize; j++) {
-				out.print(String.format("%02.3f", finalPopFitness[i][j]) + " - ");
-				for (int k = 0; k < length; k++) {
-					out.print(finalPopulations[i][j][k] + " ");
-				}
-				out.println("");
+		out.println("Final Population\n");
+
+		for (int j = 0; j < POPSIZE; j++) {
+			out.print(String.format("%02.3f", finalPopFitness[j]) + " - ");
+			for (int k = 0; k < GENE_NUMBER; k++) {
+				out.print(finalPopulation[j][k] + " ");
 			}
-			out.println("Average - " + finalPopFitness[i][popSize] + "\n");
+			out.println("");
 		}
+		out.println("Average - " + finalPopFitness[POPSIZE] + "\n");
+
 		out.close();
-		
+
 		fstream = new FileWriter("Generations Fitness.txt");
 		out = new PrintWriter(fstream);
 
 		out.println("Generations Fitness\n");
-		
-		for (int i = 0; i < generations+1; i=i+50) {
+
+		for (int i = 0; i < GENERATIONS+1; i=i+50) {
 			out.println(avgFitness[i]);
 		}
 		out.close();
-		
+
 		System.out.println("Finished");
 	}
 
@@ -105,9 +107,9 @@ public class GeneticAlgorithm {
 	 * Prints the current population and fitness to the file "out.txt"
 	 **/
 	private void printPop() {
-		for (int i = 0; i < popSize; i++) {
+		for (int i = 0; i < POPSIZE; i++) {
 			out.print(String.format("%02.3f", popFitness[i]) + " - ");
-			for (int j = 0; j < length; j++) {
+			for (int j = 0; j < GENE_NUMBER; j++) {
 				out.print(population[i][j] + " ");
 			}
 			out.println("");
@@ -121,16 +123,16 @@ public class GeneticAlgorithm {
 	 **/
 	private void setup() {
 		// Create initial random population
-		population = new int[popSize][length];
-		intPopulation = new int[popSize][length];
-		popFitness = new double[popSize];
-		avgFitness = new double[generations+1];
+		population = new Double[POPSIZE][GENE_NUMBER];
+		intPopulation = new Double[POPSIZE][GENE_NUMBER];
+		popFitness = new long[POPSIZE];
+		avgFitness = new long[GENERATIONS+1];
 		gen = 0;
 		fitTotal = 0;
 
-		for (int i = 0; i < popSize; i++) {
-			for (int j = 0; j < length; j++) {
-				population[i][j] = rand.nextInt(2);
+		for (int i = 0; i < POPSIZE; i++) {
+			for (int j = 0; j < GENE_NUMBER; j++) {
+				population[i][j] = rand.nextDouble();
 			}
 		}
 		calcFitness();
@@ -145,41 +147,12 @@ public class GeneticAlgorithm {
 		//roulette();		
 		crossover();
 		mutate();
-		population = new int[popSize][length];
-		for (int i = 0; i < popSize; i++) {
-			System.arraycopy(intPopulation[i],0,population[i],0,length);
+		population = new Double[POPSIZE][GENE_NUMBER];
+		for (int i = 0; i < POPSIZE; i++) {
+			System.arraycopy(intPopulation[i],0,population[i],0,GENE_NUMBER);
 		}
-		
+
 		calcFitness();
-	}
-
-	/** 
-	 * Calculates the fitness based on the fitness method.
-	 **/
-	private void calcFitness() {
-		fitTotal = 0;
-		for (int i = 0; i < popSize; i++) {
-			popFitness[i] = fitness(population[i]);
-			fitTotal += popFitness[i];
-		}
-		avgFitness[gen] = fitTotal/popSize;
-	}
-
-	/** 
-	 * Selects the intermediate population using the Roulette wheel method.
-	 **/
-	private void roulette() {
-		for (int i = 0; i < popSize; i++) {
-			double sum = 0;
-			int choose = rand.nextInt((int)fitTotal+1);
-			for (int j = 0; j < popSize; j++) {
-				sum += popFitness[j];
-				if (sum > choose) {
-					System.arraycopy(population[j],0,intPopulation[i],0,length);
-					break;
-				}
-			}			
-		}
 	}
 
 	/** 
@@ -187,12 +160,12 @@ public class GeneticAlgorithm {
 	 **/
 	private void SUSampling() {
 		int j = 0;
-		double sum = rand.nextDouble()*fitTotal/popSize; // Choose a random starting place
-		for (int i = 0; i < popSize; i++) {
+		double sum = rand.nextDouble()*fitTotal/POPSIZE; // Choose a random starting place
+		for (int i = 0; i < POPSIZE; i++) {
 			sum += popFitness[i];
-			while (sum > fitTotal/popSize && j < popSize) {
-				System.arraycopy(population[i],0,intPopulation[j],0,length);
-				sum -= fitTotal/popSize;
+			while (sum > fitTotal/POPSIZE && j < POPSIZE) {
+				System.arraycopy(population[i],0,intPopulation[j],0,GENE_NUMBER);
+				sum -= fitTotal/POPSIZE;
 				j++;
 			}
 		}
@@ -202,11 +175,11 @@ public class GeneticAlgorithm {
 	 * Selects each pair of chromosome's and performs crossover at a random point.
 	 **/
 	private void crossover() {
-		for (int i = 0; i < popSize; i++) {
-			if (rand.nextInt((int) (1/crossProb)) == 0) {
-				int cross1 = rand.nextInt(popSize);
-				int cross2 = rand.nextInt(popSize);
-				int crossoverPoint = rand.nextInt(length);
+		for (int i = 0; i < POPSIZE; i++) {
+			if (rand.nextInt((int) (1/CROSSOVER_PROB)) == 0) {
+				int cross1 = rand.nextInt(POPSIZE);
+				int cross2 = rand.nextInt(POPSIZE);
+				int crossoverPoint = rand.nextInt(GENE_NUMBER);
 				int[] tmp = new int[crossoverPoint];
 				System.arraycopy(intPopulation[cross1], 0, tmp, 0, crossoverPoint);
 				System.arraycopy(intPopulation[cross2], 0, intPopulation[cross1], 0, crossoverPoint);
@@ -219,87 +192,105 @@ public class GeneticAlgorithm {
 	 * Loops over each gene and mutates it with a probability determined by mutateProb.
 	 **/
 	private void mutate() {
-		for (int i = 0; i < popSize; i++) {
-			for (int j = 0; j < length; j++) {
-				if (rand.nextInt((int) (1/mutateProb)) == 0) {
-					if (intPopulation[i][j] == 1) {
-						intPopulation[i][j] = 0;
+		for (int i = 0; i < POPSIZE; i++) {
+			for (int j = 0; j < GENE_NUMBER; j++) {
+				if (rand.nextInt((int) (1/MUTATE_PROB)) == 0) {
+					if (intPopulation[i][j] == 1D) {
+						intPopulation[i][j] = 0D;
 					} else {
-						intPopulation[i][j] = 1;
+						intPopulation[i][j] = 1D;
 					}
 				}
 			}
 		}
 	}
-
+	
 	/** 
-	 * Calculates a chromosome's fitness by averaging the results of playing 
-	 * against each chromosome in the population. 
+	 * Calculates the fitness based on the fitness method.
 	 **/
-	private double fitness(int[] chromosome) {
-		double total = 0;
-		for (int i = 0; i < popSize; i++) {
-			total += play(chromosome, population[i]);
+	private void calcFitness() {
+		fitTotal = 0;
+		
+		for (int i = 0; i < POPSIZE; i++) {
+			popFitness[i] = fitnessAll(population[i]);
+			fitTotal += popFitness[i];
 		}
-
-		return total/popSize;
+		avgFitness[gen] = fitTotal/POPSIZE;
+	}
+	
+	/** 
+	 * Calculates the fitness by running games against each individuals closest neighbours.
+	 * Each game is run in its own thread.
+	 **/
+	private void calcFitnessLimited() {
+		fitTotal = 0;
+		HashMap<Integer[], Future<Long>> results = new HashMap<Integer[], Future<Long>>();
+		HashMap<Integer, HashSet<Integer>> alreadyPlayed = new HashMap<Integer, HashSet<Integer>>();
+		for (int i = 0; i < POPSIZE; i++) {
+			for (int j = 0; j < NEIGHBOUR_NUMBER; j++) {
+				int index = j - (NEIGHBOUR_NUMBER-1)/2;
+				/* Limit index to POPSIZE */
+				if (index < 0) index = POPSIZE+index;
+				if (index > POPSIZE) index = index - POPSIZE;
+				
+				HashSet<Integer> currPlayed = alreadyPlayed.get(i);
+				
+				if (currPlayed != null && currPlayed.contains(index))
+					continue;
+				
+				if (currPlayed == null)
+					currPlayed = new HashSet<Integer>();
+					
+				currPlayed.add(index);
+				alreadyPlayed.put(i, currPlayed);
+				
+				
+				// TODO: Test if the match has already been played  //if (alreadyPlayed.containsKey(i) && alreadyPlayed.get(i));
+				Callable<Long> game = new Game(new AINeuralNetwork(population[i]), new AINeuralNetwork(population[j]));
+				popFitness[i] = fitnessAll(population[i]);
+				fitTotal += popFitness[i];
+			}
+		}
+		
+		avgFitness[gen] = fitTotal/POPSIZE;
 	}
 
 	/** 
-	 * Plays p1 against p2 a number times (determined by games variable) and averages the result.
-	 * @param p1 The int array representing the chromosome of player 1.
-	 * @param p2 The int array representing the chromosome of player 2.
+	 * Calculates an individual's fitness by averaging the results of playing 
+	 * against each individual in the population. 
+	 **/
+	private long fitnessAll(Double[] chromosome) {
+		long total = 0;
+		for (int i = 0; i < POPSIZE; i++) {
+			total += play(chromosome, population[i]);
+		}
+
+		return total/POPSIZE;
+	}
+
+	/** 
+	 * Calculates an individual's fitness by averaging the results of playing 
+	 * against each individual within a set distance of itself.
+	 * The distance is determined by the constant NEIGHBOUR_NUMBER
+	 **/
+	private long fitnessLimited(Double[] chromosome) {
+		long total = 0;
+		for (int i = 0; i < POPSIZE; i++) {
+			total += play(chromosome, population[i]);
+		}
+
+		return total/POPSIZE;
+	}
+
+	/** 
+	 * Plays p1 against p2 a number times (determined by games constant) and averages the result.
+	 * @param p1 The float array representing the chromosome of individual 1.
+	 * @param p2 The float array representing the chromosome of individual 2.
 	 * @return Returns the average score of p1 against p2.
 	 **/
-	private double play(int[] p1, int[] p2) {
-		int previous1;
-		int previous2;
-		double score1 = 0;
-
-		String tPrev = "";
-		for (int i = length-2*memory; i < length; i++) {
-			tPrev += p1[i];
-		}
-		previous1 = Integer.parseInt(tPrev, 2);
-		
-		tPrev = "";
-		for (int i = length-2*memory; i < length; i++) {
-			tPrev += p2[i];
-		}
-		previous2 = Integer.parseInt(tPrev, 2);
-
-		for (int i = 0; i < games; i++) {
-			int move1 = p1[previous1];
-			int move2 = p2[previous2];
-
-			if (move1 + move2 == 2) { // both cooperate
-				score1 += C;
-			} else if (move1 > move2) { // p1 cooperates, p2 doesn't
-				score1 += S;
-			} else if (move1 < move2) {
-				score1 += T;
-			} else if (move1 + move2 == 0) { // both defect
-				score1 += D;
-			}
-
-			// Calculate previous move
-			previous1 = previous1 << 2; // Clear last bits
-			int test1 = (int) Math.pow(2, memory*2 + 1);
-			if (previous1 > test1 - 1) previous1 -= test1 ; // Remove extra bits
-			if (previous1 > length-2*memory - 1) previous1 -= length-2*memory;
-			previous1 += move1*2;
-			previous1 += move2;
-
-			previous2 = previous2 << 2; // Clear last bits
-			int test2 = (int) Math.pow(2, memory*2 + 1);
-			if (previous2 > test2 - 1) previous2 -= test2 ; // Remove extra bits
-			if (previous2 > length-2*memory - 1) previous2 -= length-2*memory;
-			previous2 += move2*2;
-			previous2 += move1;
-
-		}
-
-		return score1/games;
+	private long play(Double[] p1, Double[] p2) {
+		//TODO: Run simulated games in threads
+		return 0;
 	}
 
 }
